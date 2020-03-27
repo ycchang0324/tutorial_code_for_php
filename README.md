886c41324e94d65a96a2fbd7d1254c2da7dd381b
Justin Chang <ycchang0324@gmail.com>
2020-03-27 17:40:43 +0800

    good

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 #This is for php tutorial
-##Good luck
+#Good luck
---
+++ tutorial_11.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+<h1>Welcome to my home page!</h1>
+<?php include 'vars.php';
+echo "I have a $color $car.";
+?>
+    </body>
+</html>
